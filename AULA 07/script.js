9ef079b71f74a843ec1b2a0393e02e61a29cc120
unknown <pedro@gmail.com>
2026-06-02 09:14:06 -0300

    aula 07

---
+++ AULA 07/script.js
@@ -0,0 +1,14 @@
+let nome = document.querySelector('#nome').value
+
+function exibirNome() {
+    const userInput = document.getElementById("nome").value
+
+    alert(userInput)
+}
+
+
+function somar(){
+    let valA = Number(document.getElementById("#valorA").value)
+    let valB = Number(document.getElementById("#valorB").value)
+    alert(valA + valB)
+}
